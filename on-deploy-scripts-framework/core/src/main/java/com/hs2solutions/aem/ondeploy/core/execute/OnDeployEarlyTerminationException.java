a3e4d853d3c1dc09811fa09e633c3e80adc963d9
Brett Birschbach <brett.birschbach@hs2solutions.com>
2018-04-13 11:35:57 -0500

    This feature is now offically part of [ACS AEM Commons](https://adobe-consulting-services.github.io/acs-aem-commons/features/on-deploy-scripts/index.html)

--- on-deploy-scripts-framework/core/src/main/java/com/hs2solutions/aem/ondeploy/core/execute/OnDeployEarlyTerminationException.java
+++
@@ -1,7 +0,0 @@
-package com.hs2solutions.aem.ondeploy.core.execute;
-
-public class OnDeployEarlyTerminationException extends RuntimeException {
-    public OnDeployEarlyTerminationException(Throwable cause) {
-        super("On-deploy scripts terminated due to a fatal error. One or more on-deploy scripts have not been run, and will not be run again until next deployment.", cause);
-    }
-}
--- on-deploy-scripts-framework/core/src/main/java/com/hs2solutions/aem/ondeploy/core/execute/OnDeployExecutor.java
+++
@@ -1,7 +0,0 @@
-package com.hs2solutions.aem.ondeploy.core.execute;
-
-/**
- * A service that triggers scripts on deployment to an AEM server.
- */
-public interface OnDeployExecutor {
-}
